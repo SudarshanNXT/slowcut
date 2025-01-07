a9556104bb60d7c37bdc691f88260c90eb45d99c
Diego Arriagada <arriagada689@gmail.com>
2025-01-06 21:02:03 -0500

    router

--- letterboxd-frontend/src/App.jsx
+++ letterboxd-frontend/src/App.jsx
@@ -1,11 +1,28 @@
-import { useState } from 'react'
+import { 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  Route, 
+  RouterProvider 
+} from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext.jsx'
+import MainLayout from './layouts/MainLayout'
+import HomePage from './pages/HomePage'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />}/>
+      {/* <Route path='/login' element={<Login />}/> */}
+    </Route>
+  )
+)
 
 function App() {
 
   return (
-    <>
-      <div className='bg-blue-500'>hello world</div>
-    </>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   )
 }
 
