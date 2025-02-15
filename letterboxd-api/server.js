1ea1a18b1bc11b0f32da9a4089ac2c0646ae941b
Diego Arriagada <arriagada689@gmail.com>
2025-02-15 14:39:49 -0500

    cors fix

--- letterboxd-api/server.js
+++ letterboxd-api/server.js
@@ -19,9 +19,10 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.use(cors({
-    origin: ['https://letterboxd-clone-frontend.vercel.app', 'http://localhost:5173', 'http://localhost:4173']
-}));
+// app.use(cors({
+//     origin: ['https://letterboxd-clone-frontend.vercel.app', 'http://localhost:5173', 'http://localhost:4173']
+// }));
+app.use(cors())
 
 app.use(cookieParser())
 
